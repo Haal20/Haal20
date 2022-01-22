5f6042cb2813572656ad1cfc40ee3ff5602688aa
Haal20 <alma-harlin@hotmail.com>
2022-01-22 09:30:24 +0100

    uppdated CV, profPic, backPic and animations

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function HomeView() {
     <a href="mailto:alma-harlin@hotmail.com"><i class="far fa-envelope"></i> email</a>
     <a href="https://steamcommunity.com/id/Abokiddie/"><i class="fab fa-steam-symbol"></i> steam</a>
     <a href="https://www.facebook.com/Almster.The.Hamster/"><i class="fab fa-facebook-f"></i> facebook</a>
-    <a href="https://www.instagram.com/almaharlin0.1/"><i class="fab fa-instagram"></i> instagram</a>
+    <a href="https://www.instagram.com/haal0.2/"><i class="fab fa-instagram"></i> instagram</a>
     <a href="https://github.com/Haal20"><i class="fab fa-github"></i> github</a>
     <a href="https://www.linkedin.com/in/alma-h%C3%A4rlin-93b36116a/"><i class="fab fa-linkedin-in"></i> linkedin</a>
     <h1>Don't be shy, say hi <i class="fas fa-level-up-alt"></i></h1>
@@ -48,13 +48,14 @@ webbpages.addEventListener('click', (e) => {
     main.innerHTML = `<div>
     <h2>Webbpages I manage</h2>
     <a href="https://www.vaxjoboxningsklubb.se/"><i class="fas fa-link"></i> Växjö Boxningsklubb</a> <br>
-    <a href="https://fcvaxjo.se/"><i class="fas fa-link"></i> FC Växjö</a>
+    <a href="https://fcvaxjo.se/"><i class="fas fa-link"></i> FC Växjö</a> <br>
+    <a href="http://128.199.51.186:3000/"><i class="fas fa-link"></i> 16% SUMO (local musician in Växjö)</a>
   </div>`;
 });
 
 cv.addEventListener('click', (e) => {
     main.innerHTML = `<div>
     <h2>Education/Experience</h2>
-    <p>Open a PDF file: <a href="./images/HaalCV2021.pdf">MyCV</a></p>
+    <p>Open a PDF file: <a href="./images/almaCV_2022_eng.pdf">MyCV</a></p>
   </div>`;
 });
