941e9e7f8e7187ba8d65d7ec82bd642ea0697b7b
Haal20 <alma-harlin@hotmail.com>
2022-09-02 19:30:30 +0200

    Merge commit '2771583bbdb30f2fc22c6be1b0ca863439df0cd4'

---
+++ src/components/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import { almaColors } from '../styles/SharedStyles'
+
+export const Header = (props) => {
+  // console.log(props)
+  return (
+    <StyledHeaderContainerDiv>
+      <div>
+        Hi! Welcome to my site~❤️
+      </div>
+      <StyleFlippDiv>
+        ┬─┬ ノ( ゜-゜ノ)
+      </StyleFlippDiv>
+    </StyledHeaderContainerDiv>
+  )
+}
+
+/* Styles for components */
+const StyledHeaderContainerDiv = styled.div`
+  background-color: ${almaColors.white};
+  border-bottom: #000000 1px solid;
+  width: 80%;
+  padding: 1em 0em 1em;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-wrap: wrap;
+`
+const StyleFlippDiv= styled.div`
+  text-shadow:  2px 2px 5px ${almaColors.grey500};
+  margin: 0.5em 1em;
+  transition: 1s;
+  &:hover{
+    text-shadow: 2px 2px 5px ${almaColors.black};
+    transform: scale(1.5);
+  }
+`
